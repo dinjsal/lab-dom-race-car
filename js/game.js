74e68e7441ae00b4de8c07742ef4d300bdf0a70e
Dinah Salazar <dinahsalazar@gmail.com>
2023-09-15 04:34:47 +0900

    Update iteration 1

--- js/game.js
+++ js/game.js
@@ -1,3 +1,40 @@
 class Game {
-    // code to be added
+    constructor () {
+        this.startScreen = document.querySelector('#game-intro');
+        this.gameScreen = document.querySelector('#game-screen');
+        this.gameEndScreen = document.querySelector('#game-end');
+        this.player = null;
+        this.height = 600;
+        this.width = 500;
+        this.obstacles = [];
+        this.score = 0;
+        this.lives = 3;
+        this.gameIsOver = false;
+    }
+
+    start() {
+        this.gameScreen.height = `${this.height}px`;
+        this.gameScreen.width = `${this.width}px`;
+
+        this.startScreen.style.display = 'none';
+
+        this.gameScreen.style.display = 'block';
+
+        this.gameLoop();
+    }
+
+    gameLoop() {
+        if (this.gameIsOver) return;
+
+        this.update();
+
+        window.requestAnimationFrame(() => {
+            this.gameLoop();
+        })
+    }
+
+    update() {
+        //empty for now
+    }
+
 }
